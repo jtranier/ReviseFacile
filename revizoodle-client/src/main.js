d40de1f2bbd0b57318253f12c182cb5917e7df6e
John Tranier <john.tranier@ticetime.com>
2021-06-12 12:19:13 +0200

    #1 Homepage

--- revizoodle-client/src/main.js
+++ revizoodle-client/src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import './assets/styles/font-Raleway.css'
+import './assets/styles/normalize.css'
+import './assets/styles/skeleton.css'
+import './assets/styles/extensionBoutons.css'
+import './assets/styles/boutonsAdele.css'
+
 Vue.config.productionTip = false
 
 new Vue({
