9916d9ffa3209dd70fef6ec1a247b6a63b7eafa5
John Tranier <john.tranier@ticetime.com>
2021-08-10 17:58:58 +0200

    Setup Diffusion controller (with findByTheme action)

--- revizoodle-server/server.js
+++ revizoodle-server/server.js
@@ -21,6 +21,8 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to Revizoodle application." });
 });
 
+require('./app/routes/diffusion.routes')(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
